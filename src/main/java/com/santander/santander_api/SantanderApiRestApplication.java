282ca268120fff0cd69dd605031878271a5bdf43
danichagas <chagasdaniel788@gmail.com>
2024-07-29 15:23:14 -0300

    Initial commit

---
+++ src/main/java/com/santander/santander_api/SantanderApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.santander.santander_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderApiRestApplication.class, args);
+	}
+
+}
